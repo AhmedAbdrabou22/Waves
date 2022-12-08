62e8b187136cd1f1b2b81a7e0e78c70253edb9b4
AhmedAbdrabou22 <ahmedabdrabou723@gmail.com>
2022-12-08 11:19:05 +0200

    first commit

--- src/components/homeComponent.vue
+++ src/components/homeComponent.vue
@@ -1,53 +1,104 @@
 <template>
-    <div class="fullStack p-4">
-        <div class="row justify-content-around">
-            <div class="col-lg-5 col-md-6 col-sm-12">
-                <div>
-                    <p id="main">Perfect Blend of Technology & Creativity!</p>
-                    <h1 class="w-50">Full Stack Web Development Company</h1>
-                    <p class="mt-3" id="Parr">We are a promising web development company in Algeria, serving brands via
-                        result-oriented website development services and catering to digital needs. Are you looking for
-                        custom website development services ? Get in touch for a <b> free consultation</b>.</p>
-                    <div class="btns mt-5">
-                        <button class="bg-primary p-2 mx-2">Book a Meeting <i class="fa-solid fa-video"></i></button>
-                        <button class="p-2 mx-2">View Projects <i class="fa-sharp fa-solid fa-gift"></i></button>
+    <div class="home">
+        <!--fullStack-->
+        <div class="fullStack p-4">
+            <div class="row justify-content-around">
+                <div class="col-lg-5 col-md-6 col-sm-12">
+                    <div>
+                        <p id="main">Perfect Blend of Technology & Creativity!</p>
+                        <h1 class="w-50">Full Stack Web Development Company</h1>
+                        <p class="mt-3" id="Parr">We are a promising web development company in Algeria, serving brands
+                            via
+                            result-oriented website development services and catering to digital needs. Are you looking
+                            for
+                            custom website development services ? Get in touch for a <b> free consultation</b>.</p>
+                        <div class="btns mt-5">
+                            <button class="bg-primary p-2 mx-2">Book a Meeting <i
+                                    class="fa-solid fa-video"></i></button>
+                            <button class="p-2 mx-2">View Projects <i class="fa-sharp fa-solid fa-gift"></i></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5 col-md-6 col-sm-12">
+                    <div class="image">
+                        <div class="soft shadow"><span>Software Development <i class="fa-brands fa-chrome"></i> </span>
+                        </div>
+                        <div class="shop shadow"><span>Online Shop<i class="fa-solid fa-shop mx-1"></i></span></div>
+                        <div class="system shadow"><span>Learning Managment System <i
+                                    class="fa-sharp fa-solid fa-user-graduate"></i></span></div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-5 col-md-6 col-sm-12">
-                <div class="image">
-                    <div class="soft shadow"><span>Software Development <i class="fa-brands fa-chrome"></i> </span>
+        </div>
+        <!--FullStack-->
+
+        <!--developmentService-->
+        <div class="developmentService text-center w-75">
+            <div>
+                <h1>Our Full Stack Website Development Services</h1>
+                <div class="hr"></div>
+                <p>Our website development services include various features, fluid UX, among other things. This is what
+                    makes us the best custom web development firm. We infuse all the right and standard parameters for
+                    making your website lead in this highly competitive marketplace. As a top web development agency, we
+                    deliver effective web applications and websites. Our solutions meet market stands and business
+                    demands.
+                    We make it a priority to make your web application highly speedy, efficient and retain your users
+                    impressively.</p>
+            </div>
+            <div class="row mt-5">
+                <div class="col-lg-4 col-md-6 col-sm-12 p-2 pos" v-for="i in 3">
+                    <div class="text-center develope p-2">
+                        <h3 class="text-primary p-3" v-if="(i === 1)">POS Development</h3>
+                        <h3 class="text-primary p-3" v-else-if="(i === 2)">Frontend and Backend Development</h3>
+                        <h3 class="text-primary p-3" v-else="(i === 3)">eCommerce Solutions</h3>
+                        <p v-if="(i === 1)">Wichues offers you a unique platform that will help you manage the tableside
+                            servicing, food ordering & delivery, payments, billings, inventory and many more, also We
+                            provide POS development Solutions for restaurants, liquor store, salon, retail business and
+                            much more.</p>
+                        <p v-else-if="(i === 2)">Ensure a secure, scalable and feature-rich development of your
+                            products.
+                            We deliver websites that are efficient and high-performing. Our solutions ensure unmatched
+                            efficiency, performance, and best results helping you reserving a top seat amongst the
+                            competitors.</p>
+                        <p v-else>Explore the best custom web development services of the industry. We not only help
+                            your e-commerce business succeed, but also reach out to your potential audience. This helps
+                            businesses gain an edge in today’s competitive digital market.</p>
                     </div>
-                    <div class="shop shadow"><span>Online Shop<i class="fa-solid fa-shop mx-1"></i></span></div>
-                    <div class="system shadow"><span>Learning Managment System <i
-                                class="fa-sharp fa-solid fa-user-graduate"></i></span></div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="developmentService text-center w-75">
-        <div>
-            <h1>Our Full Stack Website Development Services</h1>
-            <div class="hr"></div>
-            <p>Our website development services include various features, fluid UX, among other things. This is what
-                makes us the best custom web development firm. We infuse all the right and standard parameters for
-                making your website lead in this highly competitive marketplace. As a top web development agency, we
-                deliver effective web applications and websites. Our solutions meet market stands and business demands.
-                We make it a priority to make your web application highly speedy, efficient and retain your users
-                impressively.</p>
-        </div>
-        <div class="row mt-5">
-            <div class="col-lg-4 col-md-6 col-sm-12 p-2 pos" v-for="i in 3">
-                <div class="text-center develope p-2">
-                    <h3 class="text-primary p-3" v-if="(i === 1)">POS Development</h3>
-                    <h3 class="text-primary p-3" v-else-if="(i === 2)">Frontend and Backend Development</h3>
-                    <h3 class="text-primary p-3" v-else="(i === 3)">eCommerce Solutions</h3>
-                    <p v-if="(i === 1)">Wichues offers you a unique platform that will help you manage the tableside servicing, food ordering & delivery, payments, billings, inventory and many more, also We provide POS development Solutions for restaurants, liquor store, salon, retail business and much more.</p>
-                    <p v-else-if="(i===2)">Ensure a secure, scalable and feature-rich development of your products. We deliver websites that are efficient and high-performing. Our solutions ensure unmatched efficiency, performance, and best results helping you reserving a top seat amongst the competitors.</p>
-                    <p v-else>Explore the best custom web development services of the industry. We not only help your e-commerce business succeed, but also reach out to your potential audience. This helps businesses gain an edge in today’s competitive digital market.</p>
-                </div>
+        <!--developmentService-->
+
+        <!--customizeDevelopment-->
+        <div class="customizeDevelopment">
+            <div class="custom">
+                <h2>Leading Custom <br> Web Development Company</h2>
+                <p class="mt-5"><b>Wichues</b> is a certified web app development company. Our bespoke, customer-centric
+                    web
+                    app development services cater to the digital needs of all-scale businesses. Our dedicated
+                    developers
+                    leverage the latest technologies and globally functional software that boosts businesses’ growth and
+                    earning potential. For the past 5 years, we have worked and delivered optimized business solutions
+                    adhering to their needs.</p>
+
+
+                <p class="mt-4">To date, we have accomplished 100+ projects with 70+ satisfied clients. Without issues
+                    has a
+                    team of skilled professionals. Every team member has years of experience in their field and provides
+                    the
+                    best app development solutions to every client. The dynamic of the company statistics is to
+                    constantly
+                    innovate, improve the products & services, and serve customer satisfaction.</p>
             </div>
         </div>
+        <!--customizeDevelopment-->
+
+        <!--process-->
+        <div class="process">
+            <h1>Our Development Process</h1>
+            <p class="text-primary">We believe in simplifying technology for you</p>
+        </div>
+        <!--process-->
     </div>
 </template>
 
@@ -58,6 +109,10 @@
 
 
 <style scoped>
+.home {
+    position: relative;
+}
+
 button {
     border: 1px solid transparent;
 }
@@ -172,17 +227,51 @@ h1 {
     background-color: #7a7a7a;
     margin: 10px auto;
 }
-.develope{
+
+.develope {
     border: 1px solid #000;
     border-radius: 10px;
-    padding:5px;
+    padding: 5px;
 }
-.develope p{
+
+.develope p {
     line-height: 25px;
     text-align: center;
     font-weight: 400;
 }
-.develope{
-    height:300px;
+
+.develope {
+    height: 300px;
+}
+
+.customizeDevelopment{
+    position: relative;
+}
+
+.custom {
+    background-image: url('@/assets/bg-blue.webp');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    color: white;
+    width: 90%;
+    padding: 80px;
+    border-top-left-radius: 50px;
+    border-bottom-left-radius: 50px;
+    position: absolute;
+    right:0;
+}
+
+.custom h2 {
+    font-weight: 700;
+}
+
+.process {
+    margin-top:50%;
+    text-align: center;
+}
+
+.process h2 {
+    color: #4b4f58;
 }
 </style>
